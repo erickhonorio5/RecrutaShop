489a497b4267976e8bd58d3221b3fc1efb0b31b5
Erick Honorio <erick.honorio40@gmail.com>
2024-11-13 18:45:03 -0300

    Implementação do Auth e finalização do Catalogo

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -3,11 +3,25 @@
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
-internal class CreateProductCommandHandler(IDocumentSession session)
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand> 
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Nome é requirido.");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Categoria é requirido.");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Imagem é requirida.");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Preço deve ser maior que 0");
+    }
+}
+
+internal class CreateProductCommandHandler
+    (IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
 
         var product = new Product
         {
@@ -22,7 +36,5 @@ internal class CreateProductCommandHandler(IDocumentSession session)
         await session.SaveChangesAsync(cancellationToken);
 
         return new CreateProductResult(product.Id);
-
-
     }
 }
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,49 @@
+﻿namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<UpdateCommandResult>;
+
+public record UpdateCommandResult(bool IsSuccess);
+
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(command => command.Id).NotEmpty().WithMessage("Requer ID do produto!");
+        RuleFor(command => command.Name)
+            .NotEmpty().WithMessage("Requer o nome!")
+            .Length(2, 150).WithMessage("O nome deve conter entre 2 e 150 characters");
+
+        RuleFor(command => command.Price)
+            .GreaterThan(0).WithMessage("O preço deve ser maior que zero!");
+    }
+}
+
+internal class UpdateProductCommandHandler
+    (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+    : ICommandHandler<UpdateProductCommand, UpdateCommandResult>
+{
+    public async Task<UpdateCommandResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("UpdateProductHandler.Handle called with {@Command}", command);
+
+        var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        product.Name = command.Name;
+        product.Category = command.Category;
+        product.Description = command.Description;
+        product.ImageFile = command.ImageFile;
+        product.Price = command.Price;
+
+        session.Update(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new UpdateCommandResult(true);
+    }
+}
+
